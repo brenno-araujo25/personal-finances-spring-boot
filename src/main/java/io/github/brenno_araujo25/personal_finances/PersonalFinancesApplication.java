f3040ad3873db09f746511d8ae51b9e2ba00a761
Brenno Araújo <brennocaldeira@outlook.com>
2025-03-17 16:40:10 -0300

    feat: initialize application with basic structure

---
+++ src/main/java/io/github/brenno_araujo25/personal_finances/PersonalFinancesApplication.java
@@ -0,0 +1,13 @@
+package io.github.brenno_araujo25.personal_finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/brenno_araujo25/personal_finances/PersonalFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brenno_araujo25.personal_finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
